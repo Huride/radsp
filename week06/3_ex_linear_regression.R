6c7d0236ffec18059f9679d05956c0274eb5f95f
macro <macro@m2block.com>
2020-07-17 23:08:35 +0900

    a

---
+++ week06/3_ex_linear_regression.R
@@ -0,0 +1,68 @@
+# For this project you will be doing the Bike Sharing Demand Kaggle challenge! 
+# 목적 : count 를 예측할 것입니다. 즉, 날씨에 따른 쉐어링 카운트.
+
+# 1. Read in 'bikeshare.csv' file and set it to a dataframe called bike.
+
+bike <- 
+
+
+# 2. Check the head of bike
+  
+  
+  
+# 3. scatter plot of count vs temp. Set a good alpha value. (ggplot)
+  
+  
+# 4. 문자열 시간으로 되어있는 컬럼을, R이 처리할 수 있는 시간으로 바꾸세요.
+# hint : as.POSIXct(bike$datetime)
+
+
+
+# 5. 다음은 그냥 실행하세요. 
+# 날짜별 쉐어링 카운트를 차트로 표시하는 코드.
+ggplot(bike,aes(datetime,count)) + geom_point(aes(color=temp),alpha=0.5)  + scale_color_continuous(low='#55D8CE',high='#FF6E2E') +theme_bw()
+
+
+
+# 6. temp와 count 간의 상관계수를 구하세요.
+
+
+# 7. 데이터셋을 트레이닝과 테스트셋으로 분리하세요. 80:20
+
+
+# 8. temp에 따른 count를 예측하는 리니어 리그레션 모델을 만드세요.
+
+temp.mode <- 
+  
+  
+# 9. Get the summary of the temp.model
+  
+  
+# 10. 전체 요소에 따른 count를 예측하는 모델을 만드세요.
+  
+  
+  
+# 11. 그리고 백워드 엘리미네이션을 이용해서 모델을 최적화 하세요.
+  
+  
+# 12. 최적 모델에 대해서, 테스트셋으로 예측한 결과를 보여주세요.
+
+
+# 13. 실제 결과값과, 예측한 결과값을 하나로 합쳐서 보여주세요.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ week06/4_Visualizing_Code.R
@@ -0,0 +1,40 @@
+# 비주얼라이징 위한 코드 제공.
+
+# 라이브러리를 다음처럼 설치합니다.
+# https://docs.google.com/presentation/d/1vAFR3Gut5Kc4rS_D6qTZg1oasTEoPTuXBCKgt01BJ68/edit#slide=id.g82dec18f42_0_248
+
+
+# Visualising the Training set results
+library(ElemStatLearn)
+set = training_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+# 리스판스로 설정하면, 싱글 벡터로 확률을 알려준다.
+prob_set = predict(classifier, type = 'response', newdata = grid_set)
+y_grid = ifelse(prob_set > 0.5, 1, 0)
+plot(set[, -3],
+     main = 'Logistic Regression (Training set)',
+     xlab = 'Age', ylab = 'Estimated Salary',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+
+# Visualising the Test set results
+library(ElemStatLearn)
+set = test_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+prob_set = predict(classifier, type = 'response', newdata = grid_set)
+y_grid = ifelse(prob_set > 0.5, 1, 0)
+plot(set[, -3],
+     main = 'Logistic Regression (Test set)',
+     xlab = 'Age', ylab = 'Estimated Salary',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
